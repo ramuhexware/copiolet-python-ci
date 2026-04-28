befc96fc6773fb124a05cd15a77c14d608f25a0d
Ramu Vurimi <vurimir@hexaware.com>
2026-04-28 12:23:47 +0530

    added code for sub ci

--- test_app.py
+++ test_app.py
@@ -6,7 +6,15 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+def test_subtract():
+    from app import subtract
+    assert subtract(2, 1) == 1
+    assert subtract(1, 1) == 0
+    assert subtract(0, 0) == 0
+    
+
  # add main function to run the tests
 if __name__ == "__main__":
     test_add()
-    print("All tests passed!")   
+    test_subtract()
+    print("All tests passed!")
